acaf56c40570009c297c94d19edeb83165c559ca
proemian <brian@proemial.ai>
2024-11-08 14:39:29 +0100

    fix

--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -4,15 +4,9 @@ document.getElementById("forwardButton").addEventListener("click", () => {
 	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 		const currentUrl = tabs[0].url;
 		console.log("currentUrl", currentUrl);
+		const encodedUrl = encodeURIComponent(currentUrl);
 
-		let newUrl = `https://proem.ai/news/${encodedUrl}?utm_medium=plugin&utm_campaign=news&utm_source=chrome`;
-
-		// Maximum length for HTTP URLs
-		if (newUrl.length > maxUrlLength) {
-			// Strip the least important part first
-			newUrl = newUrl.substring(0, maxUrlLength);
-		}
-		// // chrome.tabs.update(tabs[0].id, { url: newUrl });
+		const newUrl = `https://proem.ai/news/${encodedUrl}?utm_medium=plugin&utm_campaign=news&utm_source=chrome`;
 		chrome.tabs.create({ url: newUrl });
 	});
 });
